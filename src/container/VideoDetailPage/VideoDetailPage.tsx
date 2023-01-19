9c93a5808adf77ffc10a1db4d88806817aafaec9
simylein <67859356+simylein@users.noreply.github.com>
2023-01-19 13:24:08 +0100

    fix(video-streaming): back to dog auth

--- src/container/VideoDetailPage/VideoDetailPage.tsx
+++ src/container/VideoDetailPage/VideoDetailPage.tsx
@@ -1,14 +1,13 @@
 import { ReactElement, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { config } from '../../common/config/config';
 import pages from '../../common/styles/pages.module.css';
-import { videoFetch } from '../../services/video';
 import { getVideo } from '../../services/videoService';
 import { Video } from '../../types/video.type';
 import LoadingPage from '../LoadingPage/LoadingPage';
 import styles from './VideoDetailPage.module.css';
 
 const VideoDetailPage = (): ReactElement => {
-	const [vid, setVid] = useState<string>();
 	const [video, setVideo] = useState<Video>();
 
 	const { id } = useParams();
@@ -20,22 +19,11 @@ const VideoDetailPage = (): ReactElement => {
 			navigate('*');
 			return;
 		}
-
 		const fetchVideo = async (videoId: string): Promise<void> => {
 			const { data } = await getVideo(videoId);
 			setVideo(data);
-			if (video) {
-				try {
-					const { data } = await videoFetch(`/video/${video.id}/stream`);
-					setVid(data);
-				} catch (err) {
-					console.error(err);
-				}
-			}
 		};
-
 		fetchVideo(id);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [id, navigate]);
 
 	if (!video) return <LoadingPage />;
@@ -44,7 +32,7 @@ const VideoDetailPage = (): ReactElement => {
 		<main className={pages.wrapper}>
 			<h1>{video.title}</h1>
 			{/*eslint-disable-next-line jsx-a11y/media-has-caption*/}
-			<video controls className={styles.video} src={vid} />
+			<video controls className={styles.video} src={`${config.backendUrl}/video/${id}/stream`} />
 		</main>
 	);
 };
