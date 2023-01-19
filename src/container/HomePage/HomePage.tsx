54eafd53379ea26950636d7a43daacba2dfc8ccb
simylein <67859356+simylein@users.noreply.github.com>
2023-01-19 01:15:01 +0100

    feat(home-page): work in progress

---
+++ src/container/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from 'react';
+import styles from './HomePage.module.css';
+
+const HomePage = (): ReactElement => {
+	return (
+		<div className={styles.wrapper}>
+			<h1>TODO: Home Page</h1>
+		</div>
+	);
+};
+
+export default HomePage;
