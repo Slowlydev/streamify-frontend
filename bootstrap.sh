92b85cd4984d64edacbbc68c989d20837e2dd32b
simylein <67859356+simylein@users.noreply.github.com>
2023-01-20 16:01:59 +0100

    fix(bootstrap): now working

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,24 +2,24 @@
 # react bootstrap script
 
 export TERM="xterm-color"
-name="streamify-frontend"
+app="streamify-frontend"
 source .env
 export REACT_APP_PORT=${REACT_APP_PORT}
 
 if screen -list | grep -q "\.${app}"; then
-  echo "[${name}] info: quitting existing screen session..."
+  echo "[${app}] info: quitting existing screen session..."
   screen -S "${app}" -X quit 1>/dev/null
 fi
 
-echo "[${name}] info: deploying ${app} app..."
+echo "[${app}] info: deploying ${app} app..."
 screen -dmSL "${app}" -Logfile "screen.log" yarn start:prod 1>/dev/null
 
 sleep 2s
 
 if screen -list | grep -q "\.${app}"; then
-  echo "[${name}] ok: successfully deployed ${app} app"
+  echo "[${app}] ok: successfully deployed ${app} app"
   exit 0
 else
-  echo "[${name}] error: failed to deploy ${app} app"
+  echo "[${app}] error: failed to deploy ${app} app"
   exit 1
 fi
