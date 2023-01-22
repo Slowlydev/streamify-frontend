793694f8669069a10d6f44bc0c2e993efb9800eb
Slowlydev <slowly.developers@gmail.com>
2023-01-22 17:58:16 +0100

    feat: add function for creating a new comment

--- src/services/commentService.ts
+++ src/services/commentService.ts
@@ -1,8 +1,16 @@
-import { Comment } from '../types/comment.type';
+import { Comment, CreatedComment } from '../types/comment.type';
 import { FetchResponse } from '../types/fetch.type';
 import { Video } from '../types/video.type';
 import { fetch } from './fetch';
 
 export const getComments = (id: Video['id'], controller?: AbortController): Promise<FetchResponse<Comment[]>> => {
 	return fetch<Comment[]>('get', `/video/${id}/comment`, null, { signal: controller?.signal });
+};
+
+export const createComment = (
+	videoId: Video['id'],
+	newComment: { content: string },
+	controller?: AbortController,
+): Promise<FetchResponse<CreatedComment>> => {
+	return fetch<CreatedComment>('post', `/video/${videoId}/comment`, newComment, { signal: controller?.signal });
 };
