78c58b43fd67a831769356b8ca03e1f98ca2bba8
Slowlydev <slowly.developers@gmail.com>
2023-01-22 20:30:51 +0100

    refactor: starting to use formik wip

--- src/components/CommentSection/CommentSection.tsx
+++ src/components/CommentSection/CommentSection.tsx
@@ -1,9 +1,15 @@
 import { ReactElement, useState } from 'react';
+import { Formik, FormikHelpers, FormikState } from 'formik';
+import * as Yup from 'yup';
+
 import useComments from '../../hooks/useComments';
 import { useAuth } from '../../providers/AuthProvider/AuthProvider';
 import { useNotifications } from '../../providers/NotificationProvider/NotificationProvider';
+
 import { createComment } from '../../services/commentService';
+
 import { Video } from '../../types/video.type';
+
 import Button from '../Button/Button';
 import Comment from '../Comment/Comment';
 import ErrorState from '../ErrorState/ErrorState';
@@ -12,6 +18,10 @@ import ProfileImage from '../ProfileImage/ProfileImage';
 import Textarea from '../Textarea/Textarea';
 
 import styles from './CommentSection.module.css';
+
+type InitialValues = {
+	content: string;
+};
 
 type Props = {
 	video: Video;
@@ -22,17 +32,25 @@ const CommentSection = ({ video }: Props): ReactElement => {
 	const { addSuccess, addFailure } = useNotifications();
 	const { comments, isLoading, hasError } = useComments({ video });
 
-	const [newComment, setNewComment] = useState('');
-
-	const handleNewComment = async (): Promise<void> => {
+	const handleNewComment = async (values: InitialValues): Promise<void> => {
 		try {
-			await createComment(video.id, { content: newComment });
+			await createComment(video.id, { content: values.content });
 			addSuccess('Successfully added comment');
-			setNewComment('');
 		} catch (_) {
 			addFailure('Failed to add comment');
 		}
 	};
+
+	const initialValues: InitialValues = {
+		content: '',
+	};
+
+	const validationSchema = Yup.object().shape({
+		content: Yup.string()
+			.required('U need to type at least 8 characters to create a comment')
+			.min(8, 'U need to at least use 8 characters to create a comment')
+			.max(512, 'U can not create a comment longer than 512 characters'),
+	});
 
 	return (
 		<section>
@@ -42,14 +60,41 @@ const CommentSection = ({ video }: Props): ReactElement => {
 			{user ? (
 				<div className={styles.newComment}>
 					<ProfileImage user={user} />
-					<div className={styles.newCommentContent}>
-						<Textarea
-							value={newComment}
-							onChange={(e) => setNewComment(e.target.value)}
-							placeholder="Type here to post a new comment"
-						/>
-						{newComment.length > 7 && <Button onClick={handleNewComment} text="Post" color="blue" />}
-					</div>
+					<Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleNewComment}>
+						{({
+							values,
+							errors,
+							touched,
+							isValid,
+							submitCount,
+							isSubmitting,
+							handleSubmit,
+							setFieldValue,
+							setFieldTouched,
+						}) => {
+							return (
+								<form className={styles.newCommentContent} onSubmit={handleSubmit}>
+									<Textarea
+										value={values.content}
+										onChange={(e) => setFieldValue('content', e.target.value)}
+										onBlur={() => setFieldTouched('content', true)}
+										placeholder="Type here to post a new comment"
+									/>
+									{touched.content && !!errors.content && <div className={styles.validation}>{errors.content}</div>}
+
+									{values.content.length > 7 && (
+										<Button
+											type={'submit'}
+											text="Post"
+											color="blue"
+											loading={isSubmitting}
+											disabled={!isValid && (submitCount > 0 || isSubmitting)}
+										/>
+									)}
+								</form>
+							);
+						}}
+					</Formik>
 				</div>
 			) : (
 				<LoadingSpinner size="small" />
