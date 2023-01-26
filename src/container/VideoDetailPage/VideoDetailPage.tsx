9455ae22c905148def9e0639ab2130eca4f983b5
simylein <67859356+simylein@users.noreply.github.com>
2023-01-26 17:04:04 +0100

    feat(video-hot-reload): added it

--- src/container/VideoDetailPage/VideoDetailPage.tsx
+++ src/container/VideoDetailPage/VideoDetailPage.tsx
@@ -1,19 +1,18 @@
-import { ReactElement, useEffect, useState } from 'react';
+import { ReactElement, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { config } from '../../common/config/config';
 import pages from '../../common/styles/pages.module.css';
 import CommentSection from '../../components/CommentSection/CommentSection';
+import ErrorState from '../../components/ErrorState/ErrorState';
+import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
 import ProfileImage from '../../components/ProfileImage/ProfileImage';
-import { getVideo } from '../../services/videoService';
-import { Video } from '../../types/video.type';
+import useVideo from '../../hooks/useVideo';
 import { cl } from '../../utils/classnames.util';
-import LoadingPage from '../LoadingPage/LoadingPage';
 import styles from './VideoDetailPage.module.css';
 
 const VideoDetailPage = (): ReactElement => {
-	const [video, setVideo] = useState<Video>();
-
 	const { id } = useParams();
+	const { video, isLoading, hasError } = useVideo({ id });
 
 	const navigate = useNavigate();
 
@@ -22,42 +21,41 @@ const VideoDetailPage = (): ReactElement => {
 			navigate('*');
 			return;
 		}
-		const fetchVideo = async (videoId: string): Promise<void> => {
-			const { data } = await getVideo(videoId);
-			setVideo(data);
-		};
-		fetchVideo(id);
 	}, [id, navigate]);
-
-	if (!video) return <LoadingPage />;
 
 	return (
 		<main className={cl(pages.topWrapper, pages.wrapper)}>
-			{/*eslint-disable-next-line jsx-a11y/media-has-caption*/}
-			<video controls className={styles.video} src={`${config.backendUrl}/video/${id}/stream`} />
+			{isLoading && <LoadingSpinner size={'large'} />}
+			{hasError && <ErrorState size={'large'} />}
+			{video && (
+				<>
+					{/*eslint-disable-next-line jsx-a11y/media-has-caption*/}
+					<video controls className={styles.video} src={`${config.backendUrl}/video/${id}/stream`} />
 
-			<div className={styles.videoInfoContainer}>
-				<div className={styles.videoInfo}>
-					<h1>{video.title}</h1>
-					<p>{video.description}</p>
-				</div>
+					<div className={styles.videoInfoContainer}>
+						<div className={styles.videoInfo}>
+							<h1>{video.title}</h1>
+							<p>{video.description}</p>
+						</div>
 
-				<div>
-					<p>Views: {video.views}</p>
-					<p>Likes: {video.likes}</p>
-					<p>Dislikes: {video.dislikes}</p>
-				</div>
-			</div>
+						<div>
+							<p>Views: {video.views}</p>
+							<p>Likes: {video.likes}</p>
+							<p>Dislikes: {video.dislikes}</p>
+						</div>
+					</div>
 
-			<div className={styles.profileInfo}>
-				<ProfileImage user={video.user} />
-				<div>
-					<p>{video.user.username}</p>
-				</div>
-			</div>
+					<div className={styles.profileInfo}>
+						<ProfileImage user={video.user} />
+						<div>
+							<p>{video.user.username}</p>
+						</div>
+					</div>
 
-			<h2 className={styles.commentsTitle}>Comments</h2>
-			<CommentSection video={video} />
+					<h2 className={styles.commentsTitle}>Comments</h2>
+					<CommentSection video={video} />
+				</>
+			)}
 		</main>
 	);
 };
